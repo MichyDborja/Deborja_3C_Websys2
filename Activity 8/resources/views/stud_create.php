771735184afada6eb1ff43e07ea14084d4466e58
Michaella Deborja <michydmy@gmail.com>
2025-03-16 12:20:26 +0800

    Act8

---
+++ Activity 8/resources/views/stud_create.php
@@ -0,0 +1,21 @@
+<html>
+</head>
+
+<body>
+   <form action = "/create" method = "post">
+      <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
+      <table>
+         <tr>
+            <td>Name</td>
+            <td><input type='text' name='stud_name' /></td>
+         </tr>
+         <tr>
+            <td colspan = '2'>
+               <input type = 'submit' value = "Add student"/>
+            </td>
+         </tr>
+      </table>
+   </form>
+   
+</body>
+</html>
